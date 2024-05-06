020e2b32eb4bcb7a4a8bec85980ceaf8134853ce
Sihyun Kim <k4sud0n@gmail.com>
2024-05-06 07:30:28 +0000

    Fix typo

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
     </div>
 
       <div className="card card-side bg-base-100 shadow-xl">
-        <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+        <figure><img src="/" alt="Movie"/></figure>
         <div className="card-body">
           <h2 className="card-title">1117</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+          <p>설명</p>
         </div>
       </div>
       <div className="card card-side bg-base-100 shadow-xl">
-        <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+        <figure><img src="/" alt="Movie"/></figure>
         <div className="card-body">
           <h2 className="card-title">1150</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+          <p>설명</p>
         </div>
       </div>
     </div>
