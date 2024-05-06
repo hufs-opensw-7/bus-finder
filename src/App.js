f80c6f0aecadfb4d3213ee704eb3b53426821b04
Sihyun Kim <k4sud0n@gmail.com>
2024-05-06 07:29:50 +0000

    Add design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="lg:container lg:mx-auto">
+      <div className="navbar bg-primary text-primary-content">
+      <button className="btn btn-ghost text-xl">모버파</button>
+    </div>
+
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">1117</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+        </div>
+      </div>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">1150</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
