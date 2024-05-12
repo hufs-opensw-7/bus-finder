cbee5de272c87982936fe5a7fc3b15b9d9773906
sungwonnoh <jessica20730@naver.com>
2024-05-12 14:07:04 +0900

    bus infomation connecting

--- src/components/DetailPage/BusInformationCard.js
+++ src/components/DetailPage/BusInformationCard.js
@@ -1,16 +1,16 @@
-import redBusImage from '../../assets/red_bus.png';
+import redBusImage from "../../assets/red_bus.png";
 
-function BusInformationCard() {
-    return (
-        <div className="card bg-neutral my-4">
-            <div className="card-body items-center text-center">
-                <img src={redBusImage} alt="bus image" className="w-24" />
-                <h1 className="text-base lg:text-lg font-bold">1117번</h1>
-                <p className="text-sm lg:text-base">한국외대 ⟷ 강변역</p>
-                <p className="text-green-400 text-xs lg:text-sm">15분 뒤 도착 예정</p>
-            </div>
-        </div>
-    )
+function BusInformationCard({ busNumber, headingTo }) {
+  return (
+    <div className="card bg-neutral my-4">
+      <div className="card-body items-center text-center">
+        <img src={redBusImage} alt="bus image" className="w-24" />
+        <h1 className="text-base lg:text-lg font-bold">{busNumber}번</h1>
+        <p className="text-sm lg:text-base">한국외대 ⟷ {headingTo}</p>
+        <p className="text-green-400 text-xs lg:text-sm">15분 뒤 도착 예정</p>
+      </div>
+    </div>
+  );
 }
 
 export default BusInformationCard;
--- src/page/DetailPage.js
+++ src/page/DetailPage.js
@@ -1,13 +1,27 @@
-import BusInformationCard from '../components/DetailPage/BusInformationCard';
-import BusScheduleCard from '../components/DetailPage/BusScheduleCard';
+import { useParams } from "react-router-dom";
+import BusInformationCard from "../components/DetailPage/BusInformationCard";
+import BusScheduleCard from "../components/DetailPage/BusScheduleCard";
+import { useEffect, useState } from "react";
+import api from "../api/api.json";
 
 function DetailPage() {
-    return (
-        <>
-            <BusInformationCard />
-            <BusScheduleCard />
-        </>
-    )
+  const { busNumber } = useParams();
+  const [busInfo, setBusInfo] = useState();
+  useEffect(() => {
+    const bus = api.find((item) => item.bus_number === busNumber);
+    setBusInfo(bus);
+  }, [busNumber]);
+  return (
+    <>
+      {busInfo && (
+        <BusInformationCard
+          busNumber={busInfo.bus_number}
+          headingTo={busInfo.heading_to}
+        />
+      )}
+      <BusScheduleCard />
+    </>
+  );
 }
 
 export default DetailPage;
