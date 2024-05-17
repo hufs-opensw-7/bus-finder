c8b3c83ede4f91c4909ecfb4a00442be51af3979
Sihyun Kim <k4sud0n@gmail.com>
2024-05-17 04:14:28 +0000

    Dark mode as default

--- src/components/DetailPage/BusScheduleCard.js
+++ src/components/DetailPage/BusScheduleCard.js
@@ -1,38 +1,38 @@
 function BusScheduleCard({ busSchedule }) {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
+  const currentTime = new Date();
 
-    const formattedCurrentTime = `${currentHour.toString().padStart(2, '0')}:${currentMinute.toString().padStart(2, '0')}`;
+  const closestTime = busSchedule.weekday.reduce((prev, curr) => {
+    const [hourPrev, minutePrev] = prev.split(":").map(Number);
+    const [hourCurr, minuteCurr] = curr.split(":").map(Number);
+    const timePrev = new Date(0, 0, 0, hourPrev, minutePrev);
+    const timeCurr = new Date(0, 0, 0, hourCurr, minuteCurr);
+    const diffPrev = Math.abs(currentTime - timePrev);
+    const diffCurr = Math.abs(currentTime - timeCurr);
+    return diffPrev < diffCurr ? prev : curr;
+  });
 
-    const closestTime = busSchedule.weekday.reduce((prev, curr) => (
-        Math.abs(new Date(`2000-01-01T${prev}`) - currentTime) < Math.abs(new Date(`2000-01-01T${curr}`) - currentTime) ? prev : curr
-    ));
-
-    console.log(closestTime)
-
-    return (
-        <div className="card bg-neutral my-4">
-            <div className="overflow-x-auto">
-                <table className="table">
-                    <thead className="text-center">
-                        <tr>
-                            <th>평일</th>
-                            <th>주말</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-center">
-                    {busSchedule.weekday.map((weekdayTime, index) => (
-                        <tr key={index} className={weekdayTime === closestTime ? "bg-base-200" : ""}>
-                            <td>{weekdayTime}</td>
-                            <td>{index < busSchedule.weekend.length ? busSchedule.weekend[index] : ''}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card bg-neutral my-4">
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead className="text-center">
+            <tr>
+              <th>평일</th>
+              <th>주말</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {busSchedule.weekday.map((weekdayTime, index) => (
+              <tr key={index} className={weekdayTime === closestTime ? "bg-base-200" : ""}>
+                <td>{weekdayTime}</td>
+                <td>{index < busSchedule.weekend.length ? busSchedule.weekend[index] : ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default BusScheduleCard;
