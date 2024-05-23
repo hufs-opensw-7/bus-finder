792208bc661147da6b535bdf380b33ea04e56521
sungwonnoh <jessica20730@naver.com>
2024-05-23 21:54:59 +0900

    게임페이지 버튼 클릭

--- src/components/MainPage/GameCard.js
+++ src/components/MainPage/GameCard.js
@@ -1,18 +1,32 @@
-import playingGame from '../../assets/playing_game.png'
+import { useNavigate } from "react-router-dom";
+import playingGame from "../../assets/playing_game.png";
 
 function GameCard() {
-    return (
-        <div className="card bg-base-100 shadow-xl image-full my-8">
-            <figure><img src={playingGame} alt="playing game" /></figure>
-            <div className="card-body">
-            <h2 className="card-title text-lg lg:text-xl">언제올지 모르는 빨버 기다리기 지쳤다면?</h2>
-            <p className="text-sm lg:text-base">기다리면서 2048 한 판 해보자!</p>
-            <div className="card-actions justify-end">
-                <button className="btn btn-primary text-xs lg:text-sm">플레이하러 가기</button>
-            </div>
-            </div>
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/play");
+  };
+  return (
+    <div className="card bg-base-100 shadow-xl image-full my-8">
+      <figure>
+        <img src={playingGame} alt="playing game" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-lg lg:text-xl">
+          언제올지 모르는 빨버 기다리기 지쳤다면?
+        </h2>
+        <p className="text-sm lg:text-base">기다리면서 2048 한 판 해보자!</p>
+        <div className="card-actions justify-end">
+          <button
+            className="btn btn-primary text-xs lg:text-sm"
+            onClick={handleClick}
+          >
+            플레이하러 가기
+          </button>
         </div>
-    )
-};
+      </div>
+    </div>
+  );
+}
 
 export default GameCard;
