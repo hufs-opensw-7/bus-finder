30535581fb09f421997133cf848f96b1f8126a97
SangwooKim-0512 <kimsu3047@hufs.ac.kr>
2024-05-24 12:49:42 +0900

    modified 2048 first

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,47 +1,55 @@
-var 테이블 = document.getElementById(callbackfn: 'table');
+var 테이블 = document.getElementById('table');
 var 데이터 = [];
+var 점수표 = document.getElementById('score');
 
-function 초기화(){
+function 초기화() {
     var fragment = document.createDocumentFragment();
-    [1, 2, 3, 4].forEach(callbackfn:function(){
+    [1, 2, 3, 4].forEach(function() {
         var 열데이터 = [];
         데이터.push(열데이터);
-        var tr = document.createElement(tagName: 'tr');
-        [1, 2, 3, 4].foreach(callbackfn: function(){
+        var tr = document.createElement('tr');
+        [1, 2, 3, 4].forEach(function() {
             열데이터.push(0);
-            var td = document.createElement(tagName: 'td');
+            var td = document.createElement('td');
             tr.appendChild(td);
-
         });
         fragment.appendChild(tr);
     });
-    테이블.appendChild(Fragment);
+    테이블.appendChild(fragment);
 }
 
 function 랜덤생성() {
     var 빈칸배열 = [];
-    데이터.forEach(callbackfn: function(열데이터, i){
-        열데이터.forEach(function(행데이터, j){
-            if(!행데이터){
+    데이터.forEach(function(열데이터, i) {
+        열데이터.forEach(function(행데이터, j) {
+            if (!행데이터) {
                 빈칸배열.push([i, j]);
             }
         });
     });
-    console.log(빈칸배열);
-    var 랜덤칸 = 빈칸배열[Math.floor(Math.random() * 빈칸배열.length)];
-    데이터[랜덤칸[0]][랜덤칸[1]] = 2;
-    그리기();
+    if (빈칸배열.length === 0) {
+        alert('게임오버: ' + 점수표.textContent);
+        테이블.innerHTML = '';
+        초기화();
+    } else {
+        var 랜덤칸 = 빈칸배열[Math.floor(Math.random() * 빈칸배열.length)];
+        데이터[랜덤칸[0]][랜덤칸[1]] = 2;
+        그리기();
+    }
 }
 
-function 그리기(){
-    데이터.forEach(callbackfn, function(열데이터, i){
-        열데이터.forEach(function(행데이터, j){
-            if(행데이터 > 0){
+function 그리기() {
+    데이터.forEach(function(열데이터, i) {
+        열데이터.forEach(function(행데이터, j) {
+            if (행데이터 > 0) {
                 테이블.children[i].children[j].textContent = 행데이터;
-            }
-            else {
+            } else {
                 테이블.children[i].children[j].textContent = '';
             }
         });
     });
 }
+
+초기화();
+랜덤생성();
+그리기();
