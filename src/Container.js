96078388c66e4e0f13fd116f945c80ed17cc50ad
Sihyun Kim <k4sud0n@gmail.com>
2024-05-27 11:10:18 +0000

    Add 2048

--- src/Container.js
+++ src/Container.js
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import MainPage from "./page/MainPage";
 import DetailPage from "./page/DetailPage";
-import { PlayingGame } from "../2048/PlayingGame";
+import Game2048Page from "./page/Game2048Page";
 
 function Container() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/detail/:busNumber" element={<DetailPage />}></Route>
-      <Route path="/play" element={<PlayingGame />}></Route>
+      <Route path="/play" element={<Game2048Page />}></Route>
     </Routes>
   );
 }
---
+++ src/page/Game2048Page.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+function Game2048Page() {
+  return (
+    <div>
+      <table id="table"></table>
+      <div id="score"></div>
+    </div>
+  );
+}
+
+export default Game2048Page;
